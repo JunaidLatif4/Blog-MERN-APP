06e750b8fb6948e51d8e6a0e04e34c2fbc7f3ed2
Junaid Latif <junaidlatif004@gmail.com>
2021-07-30 21:30:12 +0500

    PostComponent Update & Project Deployed

--- FrontEnd/src/Components/Home.jsx
+++ FrontEnd/src/Components/Home.jsx
@@ -4,7 +4,68 @@ import Heading from "./HomeComponents/HeadingSection"
 import Post from "./HomeComponents/PostSection"
 import SideBar from './HomeComponents/SideBarSection'
 
+import img1 from '../IMG/demo_img1.jpg'
+import img2 from '../IMG/demo_img2.jpg'
+import img3 from '../IMG/demo_img3.jpg'
+import img4 from '../IMG/demo_img4.jpg'
+import img5 from '../IMG/demo_img5.jpg'
+import img6 from '../IMG/demo_img6.jpg'
+import img7 from '../IMG/demo_img7.jpg'
+import img8 from '../IMG/demo_img8.jpg'
+
 import './CSS/Home.scss'
+
+
+const postData = [
+    {
+        title: "You have to talk to someone about the same topic for 2 hours. What topic do you pick and why?",
+        date: "3 days",
+        author: "Junaid",
+        category: "Programing",
+        comments: "4",
+        img: img1
+    },
+    {
+        title: "What people think programing is vs how it actually is",
+        date: "3 days",
+        author: "Zahid",
+        category: "Programing",
+        comments: "9",
+        img: img2
+    },
+    {
+        title: "live action meme",
+        date: "7 days",
+        author: "Hasnat",
+        category: "Meme",
+        comments: "5",
+        img: img5
+    },
+    {
+        title: "Top Viewed Songs",
+        date: "13 days",
+        author: "Junaid",
+        category: "Entertainment",
+        comments: "77",
+        img: img1
+    },
+    {
+        title: "You have to talk to someone about the same topic for 2 hours. What topic do you pick and why?",
+        date: "77 days",
+        author: "Zulifqar",
+        category: "programing",
+        comments: "2",
+        img: img6
+    },
+    {
+        title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        date: "113 days",
+        author: "Shahzaib",
+        category: "Meme",
+        comments: "1",
+        img: img8
+    },
+]
 
 const Home = () => {
     return (
@@ -14,11 +75,19 @@ const Home = () => {
 
                 <div className="post_section">
                     <div className="post_body">
-                        <Post />
+                        {
+                            postData.map((data, index) => {
+                                return (
+                                    <>
+                                        <Post props={data} key={index} />
+                                    </>
+                                )
+                            })
+                        }
                     </div>
 
                     <div className="sidebar">
-                        <SideBar/>
+                        <SideBar />
                     </div>
                 </div>
             </div>
--- FrontEnd/src/Components/HomeComponents/PostSection.jsx
+++ FrontEnd/src/Components/HomeComponents/PostSection.jsx
@@ -4,28 +4,28 @@ import Img from '../../IMG/demo_img1.jpg'
 
 import './CSS/PostSection.scss'
 
-const PostSection = () => {
+const PostSection = ({ props }) => {
     return (
         <>
-        <div className="postsection_container">
-            <div className="postsection_box">
-                <div className="avater">
-                    <img src={Img} alt="ERROR" />
-                </div>
-                <div className="data">
-                    <div className="title">
-                        You have to talk to someone about the same topic for 2 hours. What topic do you pick and why?
+            <div className="postsection_container">
+                <div className="postsection_box">
+                    <div className="avater">
+                        <img src={props.img} alt="ERROR" />
                     </div>
-                    <div className="submit_info">
-                        submitted 3days ago by <span> chris </span> from <span> programing </span>
-                    </div>
-                    <div className="comment_info">
-                        4 comments save
-                    </div>
+                    <div className="data">
+                        <div className="title">
+                            {props.title}
+                        </div>
+                        <div className="submit_info">
+                            submitted {props.date} ago by <span> {props.author} </span> from <span> {props.category} </span>
+                        </div>
+                        <div className="comment_info">
+                            {props.comments} comments save
+                        </div>
 
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
